cb617501d5f6c44ea08318296a5c36e9e2dd9997
simran <simran@gmail.com>
2026-09-14 15:10:53 +0530

    initial commit

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -17,7 +17,7 @@ const Work = () => {
       title: 'Weatherly',
       description: 'Real-time weather, made simple.',
       image: weatherImg,
-      url: 'https://weatherlywithus.netlify.ap',
+      url: 'https://weatherlywithus.netlify.app/',
       github_url: 'https://github.com/simrankoli-01/Weather'
     },
     {
